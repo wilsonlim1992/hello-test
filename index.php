3d9f23d042c4798783632a6bf2bbc8433484db7c
wilsonlim1992 <wilson@xtremax.com>
2016-07-26 12:35:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Hello Etremax</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>Welcome to Etremax !! </h1>
+                <p>You ACloudGuru Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
